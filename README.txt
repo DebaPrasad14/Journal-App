a9e3b52f44d5f88e15a6ccaa1fad7db54fb4ab2c
unknown <mdebaprasad14@gmail.com>
2019-07-27 21:49:23 +0530

    completed app

---
+++ README.txt
@@ -0,0 +1,43 @@
+***********************************************************************************************************
+			Steps for successfull run of Journal app
+***********************************************************************************************************
+
+
+1. At first create a folder AND then put this journal-app.py file as well as this README.txt(optional).
+2. Then go to the newly created folder.
+3. In that folder, run the python file as python journal-app.py or py journal-app.py
+
+4. Then choose the options whether to register/login/exit 
+	@ options (in select option) -
+        write 1 or register - to register user
+        write 2 or login - to login user into the system
+        write 3 or exit  - to exit from the application
+
+In options user can enter either in lowercase or uppercase letter or can choose numbers provided. 
+
+5. After registering, user will be signed in to the system
+6. Then after signin user will be asked what he/she wants to do.
+
+7. After that choose the options whether to view old journals/ create new journal/ logout.
+	@ options (in select option) -
+        write 1 or view - to see previous/old journals
+        write 2 or create - to create new journal
+        write 3 or logout - to logout from the application
+
+8. Then user can ENJOY the functionality of the application.
+
+Notes: 
+	1. when user will run journal-app.py file then after registration, in the same folder accountfile.txt file will be created.
+	   (which is for storing username and passwords of users and also used for verifying username/password match or not)
+
+	2. Each time a new user registers, a separate folder will be created in the same folder
+	   And within that folder when that user creates journal a journal file will be created named username.txt
+
+	   for example - if username entered is dev, then journal file name will be dev.txt
+
+
+
+
+***************************************************************************************************************
+					END OF STEPS
+***************************************************************************************************************
---
+++ journal-app.py
@@ -0,0 +1,248 @@
+import string
+import os
+import time
+import datetime
+
+
+# Login authorization
+def loginauth(username, password):
+    '''
+    * Confirms that the username exists and that the password is correct for that username
+    * @param(str) username -- the username
+    * @param(str) password -- the password
+    * @return(bool) True -- if successful login
+    * @return(bool) False -- if unsucessful logic (either username does not exist, or password is incorrect)
+    
+    '''
+    for line in open("accountfile.txt","r").readlines(): # Read the lines
+        login_info = line.split() # Split on the space, and store the results in a list of two strings
+        if username == login_info[0] and password == login_info[1]:
+            return True
+    return False
+
+
+
+# Login a USER
+def login():
+    '''
+    @username - asking user to enter username with which they want to login
+    @password - asking user to enter password with which they want to login
+    @account.txt - file where username/passwords are stored for verification whether username/ password
+                    macthes or Not
+    @loginauth(username,password)  - function called for authentication
+    @session(username) - After successful authentication, user will b logged in to their a/c 
+    '''
+    while True:
+        username = input("Enter Username: ")
+        if not len(username) > 0:
+            print("Username can't be blank\n")
+            continue
+        else:
+            flag = False
+            for line in open("accountfile.txt","r").readlines():
+                login_info = line.split()
+                if username == login_info[0]:
+                    flag = True
+                    break
+            if not flag:
+                print("Username doesn't exist...\nKindly Enter correct Username or create new\n")
+                continue
+            break
+
+    while True:
+        password = input("Enter Your Password: ")
+        if not len(password) > 0:
+            print("Password can't be blank\n")
+        break
+
+    if loginauth(username, password):
+        return session(username)
+    else:
+        print("\nIncorrect credentials...\n")
+
+
+
+# Registering a USER
+def register():
+    '''
+    Here We are registering a new User with username and password
+    And if user doesn't enter anything in username field(means blank) then error will be shown to enter again
+    if user enters something then we will b checking whether that name is available or taken
+
+    *@accountfile.txt - storing username and password in this file for verification.
+    '''
+    while True:
+        username = input("Enter username: ")
+        if not len(username) > 0:
+            print("Username can't be blank...\n")
+            continue
+        else:
+            flag = False
+            for line in open("accountfile.txt","a+").readlines():
+                login_info = line.split()
+                if username.lower() == login_info[0].lower():
+                    print("Username already taken...\n")
+                    flag = True
+                    break
+            if flag:
+                continue
+            break
+
+
+    while True:
+        password = input("Enter Your password: ")
+        if not len(password) > 0:
+            print("Password can't be blank...\n")
+            continue
+        break
+
+    print("Creating account...")
+    acfile = open("accountfile.txt","a")
+    acfile.write(username)
+    acfile.write(" ")
+    acfile.write(password)
+    acfile.write("\n")
+    acfile.close()
+    os.makedirs(username)
+    time.sleep(1)
+    print("Account has been created!\n")
+
+    if loginauth(username, password):
+        return session(username)
+
+
+
+
+# User session
+def session(username):
+    '''
+    *@param(str) username - the username we're getting from user inputs to create a folder name as same as username
+    * Here we are asking whether to view Old journals or wants to create a new One
+    *@ options (in select option) -
+        write 1 or view - to see previous/old journals
+        write 2 or create - to create new journal
+        write 3 or logout - to logout from the application
+    '''
+    print("You are now logged in...")
+    print("Welcome to your account " + username)
+
+    if os.path.exists(username):
+        os.chdir(username)
+    else:
+        print("You haven't registered yet...")
+
+    
+    while True:
+        print("\nChoose what do You want? \n1) View Old Journals(view) \n2) Create New One(create) \n3) Logout")
+        option = input("Select Option > ").lower()
+
+        if option == "view" or option=="1":
+            view_journal(username)
+        elif option == "create" or option=="2":
+            create_journal(username)
+        elif option == "logout" or option =="3":
+            print("Logging out...")
+            break
+        else:
+            print(option + " is not a valid option...")
+
+
+def create_journal(username):
+    '''
+    Creating journal while user chooses to create journal
+    *@param(str) username  -> passing username from user inputs to create journal file
+    *@file_name -> here we're creating a journal file with the name same as username inside a folder
+                   name same as username which is specific to particular user only
+                   And adding .txt after username
+     example - if username is dev then his journal file name will become dev.txt 
+    '''
+    file_name = username+'.txt'
+    write_file(file_name)
+    print('Journal successfully created...')
+            
+
+
+def write_file(file_name):
+    '''
+    *@ param(str) file_name - the file name we created in above function
+    writing contents to the file when a user click to create
+    '''
+    with open(file_name, 'a+') as my_file:
+        content = input('Write journal:> ')
+
+        if len(content) > 0:
+            date_time = datetime.datetime.now()
+            my_file.write(date_time.strftime("%d%b%y %I.%M%p"))
+            my_file.write(' --> ')
+            my_file.write(f'{content} \r')
+            my_file.close()
+        else:
+            print('Write something in Journal...')
+
+
+
+def view_journal(username):
+    '''
+    retrieving journals written by corresponding user
+    '''
+    file_name = username+'.txt'
+    print_journals(file_name)
+
+
+def print_journals(file_name):
+    '''
+    printing the jounals when user clicks to view journal
+    '''
+    print('\n<<<<<<<<<<<<<< Start jounal >>>>>>>>>>>>>>\n')
+    read_lines(file_name)
+    print('<<<<<<<<<<<<<< End jounal >>>>>>>>>>>>>>')
+
+
+def read_lines(file_name):
+    '''
+    retrieving each lines/journals written by the user
+    checking if a single user has 50 records or more
+    if more than 50 records then show
+    '''
+    with open(file_name, 'a+') as my_file:
+        lines = my_file.readlines()
+        
+        if(len(lines)>50):
+            start_index = len(lines)-50
+        else:
+            start_index = 0
+
+        for idx, line in enumerate(lines[start_index:]):
+            print("%d) %s" % (idx+1, line))
+
+
+
+
+#Start of the program
+print("\n***********************************************************")
+print("Welcome to the Journal system. Please Register or Login!")
+
+while True:
+    '''
+    Taking inputs from user whether to register/ login /exit
+    *@ options (in select option) -
+        write 1 or register - to register user
+        write 2 or login - to login user into the system
+        write 3 or exit  - to exit from the application
+    '''
+    print("Choose What do You want ?\n1) Register \n2) Login \n3) Exit")
+    option = input("Select Option > ").lower()
+    if option == "1" or option=="register":
+        register()
+    elif option == "2" or option=="login":
+        login()
+    elif option == "3" or option=="exit":
+        break
+    else:
+        print(option + " is not a valid option")
+
+
+
+# close of application
+print("Application is Shutting down...")
+time.sleep(1)
